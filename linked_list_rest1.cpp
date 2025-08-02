d6d4f42203e199b7c4d25eb9bb0e3469b29890b3
mehedynoman11 <mehedynoman11@gmail.com>
2025-08-02 13:41:40 +0600

    print middle numbers linked list

---
+++ linked_list_rest1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node *next;
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val) {
+    Node *newNode = new Node(val);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+int get_size(Node *head) {
+    int count = 0;
+    Node *temp = head;
+    while(temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void print_middle(Node* head) {
+    int size = get_size(head);
+    int middle1 = size / 2;
+    int middle2 = (size % 2 == 0) ? middle1 - 1 : middle1;
+
+    Node *tmp = head;
+    int index = 0;
+
+    while (tmp != NULL) {
+        if (index == middle2 || index == middle1) {
+            cout<<tmp->val<<" ";
+        }
+        index++;
+        tmp = tmp->next;
+    }
+    cout<<endl;
+
+}
+
+int main() {
+    Node *head = NULL;
+    int val;
+    cout<<"ENTER VALUES -1 to STOP: ";
+    while(true) {
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head, val);
+    }
+    // cout<<"SIZE OF LINKED LIST: "<<get_size(head)<<endl;
+    print_middle(head);
+
+    return 0;
+}
--- singly_linkedList_reverse1.cpp
+++ singly_linkedList_reverse1.cpp
@@ -13,39 +13,37 @@ public:
 
 void insert_at_tail(Node *&head, int val) {
     Node *newNode = new Node(val);
-    if (head == NULL) {
+    if(head == NULL) {
         head = newNode;
         return;
     }
     Node *temp = head;
-    while (temp->next != NULL) {
+    while(temp->next != NULL) {
         temp = temp->next;
     }
     temp->next = newNode;
 }
 
-void print_recursive(Node *head) {
+void print_reversive(Node *head) {
     if (head == NULL) {
         return;
     }
-    print_recursive(head->next);
-    cout<<head->val<<" ";
+    print_reversive(head->next);
+    cout << head->val << " ";
 }
+
 
 int main() {
     Node *head = NULL;
     int val;
-    while(true) {
-        cout << "Enter a value to insert (-1 to stop): ";
-        cin>>val;
+    while (true) {
+        cin >> val;
         if (val == -1) {
             break;
         }
         insert_at_tail(head, val);
     }
-    print_recursive(head);
-    cout << endl;
-    // Clean up memory
+    print_reversive(head);
 
 
     return 0;
