14d81c3e3ea74522ef68d0512b291aa57b80b208
mehedynoman11 <mehedynoman11@gmail.com>
2025-08-19 20:03:45 +0600

    print middle in Linked list

---
+++ print_middle_Linked.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node *next;
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val) {
+    Node *newNode = new Node(val);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    } 
+    tmp->next = newNode;
+}
+
+int get_size(Node *head) {
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL) {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+void print_middle(Node *head) {
+    Node *tmp = head;
+    int size = get_size(head);
+    int middle1 = size / 2;
+    int middle2 = (size % 2 == 0) ? middle1 - 1 : middle1;
+    int index = 0;
+    while (tmp != NULL) {
+        if (index == middle2 || index == middle1) {
+            cout<<tmp->val<<" ";
+        }
+        index++;
+        tmp = tmp->next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    Node *head = NULL;
+    int val;
+    while(true) {
+        cin>>val;
+        if(val == -1) {
+            break;
+        }
+        insert_at_tail(head, val);
+    }
+    print_middle(head);
+    return 0;
+}
